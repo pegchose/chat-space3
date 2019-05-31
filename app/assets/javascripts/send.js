ae6875b4853b76b30c50482b0e2a93753f85f226
pegchose <j.c.lsevp9@gmail.com>
2019-05-31 18:27:53 +0900

    送信イベント確認

---
+++ app/assets/javascripts/send.js
@@ -0,0 +1,39 @@
+//$(function() {
+//  function buildHTML() {
+//    var html = $('<li class="todo">').append(todo.content);
+//    return html;
+//  }
+//
+//  $('.js-form').on('submit', function(e) {
+//    e.preventDefault();
+//    var textField = $('.js-form__text-field');
+//    var todo = textField.val();
+//    $.ajax({
+//      type: 'POST',
+//      url: '/todos.json',
+//      data: {
+//        todo: {
+//          content: todo
+//        }
+//      },
+//      dataType: 'json'
+//    })
+//    .done(function(data) {
+//      var html = buildHTML(data);
+//      $('.todos').append(html);
+//      textField.val('')
+//    })
+//    .fail(function() {
+//      alert('error');
+//    });
+//  });
+//});
+
+$(function() {
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    console.log("sus")
+    message = $('.form__submit').val();
+    
+  });
+});
