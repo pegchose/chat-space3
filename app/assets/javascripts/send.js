c86252323ba2abc7c134efc7a0aacc7f8be90639
pegchose <j.c.lsevp9@gmail.com>
2019-06-01 16:23:21 +0900

    FormDataを使えるようgem入れる
    このajaxだと400 bad requestになる

--- app/assets/javascripts/send.js
+++ app/assets/javascripts/send.js
@@ -1,27 +1,47 @@
+//if( window.FormData ){
+//  alert("対応しています");
+//} else {
+//  alert("対応していません");
+//}
+
+$(function(){
+  $("#new_message").on('submit',function(e){
+    e.preventDefault();
+    //var formdata = Components.classes["@mozilla.org/files/formdata;1"].createInstance(Components.interfaces.nsIDOMFormData);
+    var formdata = new FormData(this);
+    $.ajax({
+      url: "/groups/#{group_id}/messages.json",
+      type: "POST",
+      data: formdata,
+      datatype: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
+})
+
+
 //$(function() {
 //  function buildHTML() {
-//    var html = $('<li class="todo">').append(todo.content);
+//    var html = $('<div class="message">').append(message.content);
 //    return html;
 //  }
 //
-//  $('.js-form').on('submit', function(e) {
+//  $('#new_message').on('submit', function(e) {
 //    e.preventDefault();
-//    var textField = $('.js-form__text-field');
-//    var todo = textField.val();
+//    var formData = new FormData(this);
+//    var url = $('this').attr('action')
+//    var message = textField.val();
 //    $.ajax({
 //      type: 'POST',
-//      url: '/todos.json',
-//      data: {
-//        todo: {
-//          content: todo
-//        }
-//      },
+//      url: '/messages.json',
+//      data: formData,
 //      dataType: 'json'
 //    })
 //    .done(function(data) {
 //      var html = buildHTML(data);
-//      $('.todos').append(html);
-//      textField.val('')
+//      $('.messages').append(html);
+//      textField.val('') <- なにこれ
 //    })
 //    .fail(function() {
 //      alert('error');
@@ -29,11 +49,13 @@
 //  });
 //});
 
-$(function() {
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    console.log("sus")
-    message = $('.form__submit').val();
-    
-  });
-});
+    //message = $('.form__submit').val();
+    // var formdata = new Formdata(this);
+    //var url = $('this').attr('action')
+    //var message = formData.val();
+    //$.ajax({
+    //  type: 'POST',
+    //  url: "/groups/#{group_id}/messages.json",
+    //  data: formData,
+    //  dataType: 'json'
+    //})
---
+++ app/views/messages/index.json.jbuilder
@@ -0,0 +1,5 @@
+json.array! @messages do |message|
+  json.id message.id
+  json.content message.content
+  json.created_at message.created_at
+end
