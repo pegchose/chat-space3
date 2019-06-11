95d670e479115efcb91d4211415920ac3cc949da
pegchose <j.c.lsevp9@gmail.com>
2019-06-11 21:21:00 +0900

    不要なbinding消す

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,6 @@ class Api::MessagesController < ApplicationController
   def index
     @group = Group.find(params[:kgroup_id])   # kgroup_id で group_id取得
     @new_message = @group.messages.includes(:user)
-    #binding.pry
     respond_to do |format|
       format.json {@messages = @new_message.where('id > ?', "%#{params[:messageid]}%")}
       format.html 
