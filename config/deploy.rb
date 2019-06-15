3b97d3df3f2e813f359d442099124a60aef70504
pegchose <j.c.lsevp9@gmail.com>
2019-06-15 15:43:49 +0900

    unicorn.rbを'にした

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "chat-space3"
-set :repo_url, "git@github.com:pegchose/chat-space3.git"
+set :application, 'chat-space3'
+set :repo_url, 'git@github.com:pegchose/chat-space3.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
